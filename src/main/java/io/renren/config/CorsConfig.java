3cf270a3ea2ab5efdf217c90754c5fa66157249d
Admin <100742869@qq.com>
2021-06-25 17:12:46 +0800

    wjx -

--- src/main/java/io/renren/config/CorsConfig.java
+++ src/main/java/io/renren/config/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+//            .allowedOrigins("*")
+                .allowedOriginPatterns("*")
             .allowCredentials(true)
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .maxAge(3600);
